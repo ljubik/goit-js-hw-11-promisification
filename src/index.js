4f2157b21906302dd793bebc008bba36e7cefd86
Liubomyr <ljubik@ukr.net>
2020-12-16 22:30:07 +0200

    make task 2

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './styles.css';
 import './js/task1.js';
+import './js/task2.js';
--- src/js/task1.js
+++ src/js/task1.js
@@ -12,4 +12,4 @@ delay(2000).then(logger); // Resolved after 2000ms
 delay(1000).then(logger); // Resolved after 1000ms
 delay(1500).then(logger); // Resolved after 1500ms
 
-console.log("Task 1 console");
+console.log("Task 1 console after Task 2");
